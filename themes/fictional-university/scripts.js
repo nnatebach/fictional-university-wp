1187eadf216f999ae32fd2e2ecb6fb02a5d49433
FASTCODING_Nathan <minhnhan.fastcodingvn@gmail.com>
2021-11-09 17:14:23 +0700

    add asynchronous script

--- themes/fictional-university/scripts.js
+++ themes/fictional-university/scripts.js
@@ -182,17 +182,19 @@
     }
 
     getResults() {
-      // this.resultDiv.html("Something really cool goes here after 3,000 micro-second");
-      // this.isVisibleSpinner = false;
-      $.getJSON(universityData.root_url + "/wp-json/wp/v2/program?search=" + this.searchField.val(), program => { this.resultDiv.html(`
-      <h2 class="search-overlay__section-title">General Information</h2>
-      ${program.length ? '<ul class="link-list min-list">' : '<p>No general information for this search</p>'}
-        ${program.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join("")}
-      ${program.length ? '</ul>' : ''}
-      `);
+      $.when(
+        $.getJSON(universityData.root_url + "/wp-json/wp/v2/program?search=" + this.searchField.val()), 
+        $.getJSON(universityData.root_url + "/wp-json/wp/v2/posts?search=" + this.searchField.val())).then((program, posts) => {
+        var combinedResults = program[0].concat(posts[0]);
+        this.resultDiv.html(`
+        <h2 class="search-overlay__section-title">General Information</h2>
+        ${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No general information for this search</p>'}
+          ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join("")}
+        ${combinedResults.length ? '</ul>' : ''}
+        `);
         // as soon as we start with a new search, we want the "spinner" to show up
         this.isVisibleSpinner = false;
-      });
+      })
     }
 
     keyPressDispatcher(e) {
